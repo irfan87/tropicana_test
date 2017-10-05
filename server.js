99ea90e0f8ac9372b6d4d7616e4905c5b12c4494
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-10-05 15:11:33 +0800

    put body parser json

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+
 let app = express();
 
 let port = process.env.port || 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({
+        "message": "right now you can use body parser to fetch json! hooray!!!"
+    });
 });
 
 app.listen(port, (req, res) => {
